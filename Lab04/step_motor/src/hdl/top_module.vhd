86d1ec863d28b5bb05419e576175f48438cde5a7
gralerfics <glverfer@outlook.com>
2023-05-08 19:58:27 +0800

    feat(lab04): add lab04.

--- Lab04/step_motor/src/hdl/top_module.vhd
+++ Lab04/step_motor/src/hdl/top_module.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
 
 use work.constants.all;
 use work.types.all;
@@ -8,7 +10,8 @@ use work.types.all;
 entity top_module is
     port (
         clk_sys, rst: in std_logic;
-        btn_up_in, btn_down_in: in std_logic;
+        btn_left_in, btn_right_in, btn_up_in, btn_down_in: in std_logic;
+        btn_ok_in: in std_logic;
         clockwise: in std_logic;
         channels: out std_logic_vector(3 downto 0);
         anodes_n: out std_logic_vector(7 downto 0);
@@ -56,81 +59,112 @@ architecture Behavioral of top_module is
             pulse: out std_logic
         );
     end component;
+    
+    constant INPUT_MAX_LENGTH: integer := 8;
 
-    signal btn_up, btn_down: std_logic;
-    signal nums: bcd_array_t(7 downto 0);
+    signal btn_left, btn_right, btn_up, btn_down, btn_ok: std_logic;
+    signal display_pts: std_logic_vector(7 downto 0);
+    signal nums, nums_next: bcd_array_t(7 downto 0);
+    signal input_focal, input_focal_next: integer;
     signal tick_num, tick_num_next: integer;
     signal pulse: std_logic;
+    signal drv_rst: std_logic;
 begin
+    btn_left_state: button_state
+        generic map (PERIOD => 1500)
+        port map (clk => clk_sys, rst => rst, btn_in => btn_left_in, btn_out => btn_left);
+    btn_right_state: button_state
+        generic map (PERIOD => 1500)
+        port map (clk => clk_sys, rst => rst, btn_in => btn_right_in, btn_out => btn_right);
+    btn_up_state: button_state
+        generic map (PERIOD => 1500)
+        port map (clk => clk_sys, rst => rst, btn_in => btn_up_in, btn_out => btn_up);
+    btn_down_state: button_state
+        generic map (PERIOD => 1500)
+        port map (clk => clk_sys, rst => rst, btn_in => btn_down_in, btn_out => btn_down);
+    btn_ok_state: button_state
+        generic map (PERIOD => 1500)
+        port map (clk => clk_sys, rst => rst, btn_in => btn_ok_in, btn_out => btn_ok);
+    
     motor_drv: step_motor_driver
         port map (
             clk_sys => clk_sys,
-            rst => rst,
+            rst => drv_rst,
             tick_num => tick_num,
             clockwise => clockwise,
             step_table => STEPTABLE_FULLSTEP,
             channels => channels
         );
+    drv_rst <= rst or btn_ok;
 
     seven_seg_drv: seven_segments_display_driver
         port map (
             clk_sys => clk_sys,
             rst => rst,
             nums => nums,
-            pts => (others => '0'),
+            pts => display_pts,
             anodes_n => anodes_n,
             segs_n => segs_n
         );
-    
-    btn_up_state: button_state
-        generic map (
-            PERIOD => 3000
-        )
-        port map (
-            clk => clk_sys,
-            rst => rst,
-            btn_in => btn_up_in,
-            btn_out => btn_up
-        );
-
-    btn_down_state: button_state
-        generic map (
-            PERIOD => 3000
-        )
-        port map (
-            clk => clk_sys,
-            rst => rst,
-            btn_in => btn_down_in,
-            btn_out => btn_down
-        );
+    display_pts <= shl("00000001", std_logic_vector(to_unsigned(input_focal, 3)));
     
     freq_div_top: frequency_divider
         port map (
             clk => clk_sys,
             rst => rst,
-            period => 8000000,
+            period => 14285714,
             pulse => pulse
         );
     
     process (clk_sys, rst) is
     begin
         if rst = '1' then
-            tick_num <= 1047588;
+            input_focal <= 0;
+            tick_num <= 3142764;
+            nums <= (to_bcd(0), to_bcd(3), to_bcd(1), to_bcd(4), to_bcd(2), to_bcd(7), to_bcd(6), to_bcd(4));
         elsif rising_edge(clk_sys) and pulse = '1' then
+            input_focal <= input_focal_next;
             tick_num <= tick_num_next;
+            nums <= nums_next;
         end if;
     end process;
-
-    tick_num_next <= tick_num - 5000 when btn_down = '1' and tick_num /= 1 else
-                     tick_num + 5000 when btn_up = '1' else
-                     tick_num;
-
-    nums(7) <= to_bcd(tick_num / 10000000 mod 10);
-    nums(6) <= to_bcd(tick_num / 1000000 mod 10);
-    nums(5) <= to_bcd(tick_num / 100000 mod 10);
-    nums(4) <= to_bcd(tick_num / 10000 mod 10);
-    nums(3) <= to_bcd(tick_num / 1000 mod 10);
-    nums(2) <= to_bcd(tick_num / 100 mod 10);
-    nums(1) <= to_bcd(tick_num / 10 mod 10);
-    nums(0) <= to_bcd(tick_num mod 10);
+    tick_num_next <= tick_num when btn_ok = '0' else
+                     to_integer(nums(7)) * 10000000 +
+                     to_integer(nums(6)) * 1000000 +
+                     to_integer(nums(5)) * 100000 +
+                     to_integer(nums(4)) * 10000 +
+                     to_integer(nums(3)) * 1000 +
+                     to_integer(nums(2)) * 100 +
+                     to_integer(nums(1)) * 10 +
+                     to_integer(nums(0));
+    process (clk_sys, rst, btn_left, btn_right, btn_up, btn_down, input_focal) is
+    begin
+        input_focal_next <= input_focal;
+        nums_next <= nums;
+        if btn_left = '1' then
+            if input_focal = INPUT_MAX_LENGTH - 1 then
+                input_focal_next <= 0;
+            else
+                input_focal_next <= input_focal + 1;
+            end if;
+        elsif btn_right = '1' then
+            if input_focal = 0 then
+                input_focal_next <= INPUT_MAX_LENGTH - 1;
+            else
+                input_focal_next <= input_focal - 1;
+            end if;
+        elsif btn_up = '1' then
+            if nums(input_focal) = "1001" then
+                nums_next(input_focal) <= "0000";
+            else
+                nums_next(input_focal) <= nums(input_focal) + 1;
+            end if;
+        elsif btn_down = '1' then
+            if nums(input_focal) = "0000" then
+                nums_next(input_focal) <= "1001";
+            else
+                nums_next(input_focal) <= nums(input_focal) - 1;
+            end if;
+        end if;
+    end process;
 end architecture;
--- Lab04/step_motor/src/hdl/types.vhd
+++ Lab04/step_motor/src/hdl/types.vhd
@@ -8,6 +8,7 @@ package types is
 
     subtype bcd_t is std_logic_vector(3 downto 0);
     function to_bcd(x: integer) return bcd_t;
+    function to_integer(x: bcd_t) return integer;
 
     type bcd_array_t is array (natural range <>) of bcd_t;
 end package;
@@ -17,5 +18,10 @@ package body types is
     function to_bcd(x: integer) return bcd_t is
     begin
         return std_logic_vector(to_unsigned(x, 4));
+    end function;
+
+    function to_integer(x: bcd_t) return integer is
+    begin
+        return to_integer(unsigned(x));
     end function;
 end package body;
